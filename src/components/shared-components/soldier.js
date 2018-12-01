b9c1be3ccc4f9f745dda70f56455717afde52fc0
Nguyen Minh Trung <tuxdomatna@gmail.com>
2018-12-01 10:20:47 +0700

    soldier component

--- src/components/shared-components/soldier.js
+++ src/components/shared-components/soldier.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Soldier = ({ type, quantity }) => (
-  <div className="soldier">
-
+const Soldier = ({ type, quantity, onClick }) => (
+  <div className={`soldier ${type}`} onClick={() => onClick()}>
+    <div className="quantity">
+      {quantity}
+    </div>
   </div>
 );
 
 Soldier.propTypes = {
   type: PropTypes.string,
   quantity: PropTypes.number,
+  onClick: PropTypes.func,
 };
 
 export default Soldier;
