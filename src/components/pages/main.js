dc681536b6bc2db2229709c20947d67c72e45b2d
duynhan <duynhan.nguyenhoang@gmail.com>
2018-12-01 11:12:48 +0700

    add user section

--- src/components/pages/main.js
+++ src/components/pages/main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Soldier from '../shared-components/soldier';
 
 const Main = () => (
-  <div className="main">
+  <div className="main py-4">
     <div className="container">
-      <section className="tower-info mb-4">
+      <section className="align-items-center d-flex mb-4 tower-info">
         <div className="tower-avatar" style={{backgroundImage: 'url(/images/sand-castle.png)'}}></div>
-        <div className="d-flex flex-column">
-          <div className="health-bar" style={{ backgroundColor: '#FF3636'}}></div>
+        <div className="d-flex flex-column ml-2">
+          <div className="health-bar">
+            <div style={{ backgroundColor: '#FF3636', width: '80%' }}></div>
+          </div>
           <div className="username">Username 1</div>
         </div>
       </section>
