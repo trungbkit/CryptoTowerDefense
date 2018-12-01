fa2c044ffe2f569bfd94e9884d481277f99341f5
duynhan <duynhan.nguyenhoang@gmail.com>
2018-12-01 09:24:20 +0700

    them hinh

--- src/components/pages/main.js
+++ src/components/pages/main.js
@@ -2,15 +2,7 @@ import React from 'react';
 
 const Main = () => (
   <div className="main">
-    <div className="col">
-      <div className="title">Users</div>
-    </div>
-    <div className="col">
-      <div className="title">Albums</div>
-    </div>
-    <div className="col">
-      <div className="title">Photos</div>
-    </div>
+    
   </div>
 );
 
