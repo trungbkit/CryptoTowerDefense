f3aee2f3c83a0dd78c308130ea9d369ea20c8df7
duynhan <duynhan.nguyenhoang@gmail.com>
2018-12-01 10:25:28 +0700

    fix bootstrap

--- src/components/pages/main.js
+++ src/components/pages/main.js
@@ -3,7 +3,16 @@ import Soldier from '../shared-components/soldier';
 
 const Main = () => (
   <div className="main">
-    <Soldier type="earth" quantity={4} />
+    <div className="container">
+      <section className="tower-info mb-4">
+        <div className="tower-avatar" style={{backgroundImage: 'url(/images/sand-castle.png)'}}></div>
+        <div className="d-flex flex-column">
+          <div className="health-bar" style={{ backgroundColor: '#FF3636'}}></div>
+          <div className="username">Username 1</div>
+        </div>
+      </section>
+      <Soldier type="earth" quantity={4} />
+    </div>
     {/* <Soldier type="fire" quantity={4} />
     <Soldier type="water" quantity={4} />
     <Soldier type="wood" quantity={4} />
