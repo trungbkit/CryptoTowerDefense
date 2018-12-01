1fe9bbf6cf80cbd7b5d87498685acbbc625717e0
Nguyen Minh Trung <tuxdomatna@gmail.com>
2018-12-01 09:43:28 +0700

    use soldier

--- src/components/pages/main.js
+++ src/components/pages/main.js
@@ -4,6 +4,10 @@ import Soldier from '../shared-components/soldier';
 const Main = () => (
   <div className="main">
     <Soldier type="earth" quantity={4} />
+    {/* <Soldier type="fire" quantity={4} />
+    <Soldier type="water" quantity={4} />
+    <Soldier type="wood" quantity={4} />
+    <Soldier type="metal" quantity={4} /> */}
   </div>
 );
 
